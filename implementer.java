14b32ee15a5b43d5299822f8caa7dcfe9383b688
AngelLeoKim <161600326+AngelLeoKim@users.noreply.github.com>
2024-02-28 20:48:59 +0800

    Add files via upload

---
+++ implementer.java
@@ -0,0 +1,40 @@
+public class implementer extends student {
+
+    public static void main(String[] args) {
+
+        student s1 = new student();
+        s1.name("Jhon Tormis");
+        s1.add("Taguibo, Butuan City");
+        s1.num("09126578459");
+        s1.course1("Discete Math");
+        s1.course2("MMW");
+
+        student s2 = new student();
+        s2.name("John Cena");
+        s2.add("Antongalon, Butuan City");
+        s2.num("09549785468");
+        s2.course1("OOP");
+        s2.course2("Web Technologies");
+
+        student s3 = new student();
+        s3.name("Maria  Nicole");
+        s3.add("Basag, Butuan City");
+        s3.num("09565587421");
+        s3.course1("Integrative Programming 1");
+        s3.course2("Integrative Programming 2");
+
+        student s4 = new student();
+        s4.name("Ben Tennyson");
+        s4.add("Villa Kananga, Butuan City");
+        s4.num("09478962121");
+        s4.course1("Data Structures");
+        s4.course2("Algorithms");
+
+        student s5 = new student();
+        s5.name("Nicole Havan");
+        s5.add("Taligaman, Butuan City");
+        s5.num("09946789541");
+        s5.course1("Software Engineer 1");
+        s5.course2("Human Computer Interaction");
+    }
+}
