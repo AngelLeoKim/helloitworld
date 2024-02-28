4f7d90d1b567d0d72e9a382d1e69fd718034cddb
AngelLeoKim <161600326+AngelLeoKim@users.noreply.github.com>
2024-02-28 20:49:21 +0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,28 @@
+public class student {
+
+    String name;
+    String add;
+    String num;
+    String course1;
+    String course2;
+
+    public void name(String name) {
+        System.out.println("Name: " + name);
+    }
+
+    public void add(String add) {
+        System.out.println("Address: " + add);
+    }
+
+    public void num(String num) {
+        System.out.println("Contact Number: " + num);
+    }
+
+    public void course1(String course1) {
+        System.out.println("Enrolled Course #1: " + course1);
+    }
+
+    public void course2(String course2) {
+        System.out.println("Enrolled Course #2: " + course2);
+    }
+}
